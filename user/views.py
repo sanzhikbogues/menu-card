39f59b35ccd87dc699ba94479ecd4e954270ba40
TAbilmansur <156643282+TAbilmansur@users.noreply.github.com>
2025-04-25 22:08:02 +0500

    Add files via upload

---
+++ user/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSignUpSerializer
+
+
+
+# Create your views here.
+class UserSignUpAPIView(APIView):
+    def post(self, request):
+        serializer = UserSignUpSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
